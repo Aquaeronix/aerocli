34a4f86781b4a4e0cee2bebe08797b263f9acdb4
Dennis Chen <barracks510@gmail.com>
2015-01-06 19:29:06 -0800

    configure SNMP modules

--- utilities/SNMP/snmp_persist_aquaero5.pl
+++ utilities/SNMP/snmp_persist_aquaero5.pl
@@ -2,29 +2,33 @@
 #
 # snmp_persist_aquaero5.pl
 #
-# Export the Aquaero 5 sensor readings and settings provided ny aerotools-ng to net-snmp via 
+# Export the Aquaero 5 sensor readings and settings provided ny Aquaeronix to net-snmp via 
 # pass_persist for external consumption.
 #
 # Copyright 2013 JinTu <JinTu@praecogito.com>
+# Copyright 2014 barracks510 <barracks510@gmail.com>
+# Copyright 2015 barracks510 <barracks510@gmail.com>
 #
 #
-# This file is a support utility of aerotools-ng.
+# This file is a support utility of Aquaeronix.
 #
 # Copyright 2012 lynix <lynix47@gmail.com>
 # Copyright 2013 JinTu <JinTu@praecogito.com>, lynix <lynix47@gmail.com>
+# Copyright 2014 barracks510 <barracks510@gmail.com>
+# Copyright 2015 barracks510 <barracks510@gmail.com>
 #
-# aerotools-ng is free software: you can redistribute it and/or modify
+# Aquaeronix is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# aerotools-ng is distributed in the hope that it will be useful,
+# Aquaeronix is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with aerotools-ng. If not, see <http://www.gnu.org/licenses/>.
+# along with Aquaeronix. If not, see <http://www.gnu.org/licenses/>.
 #
 ####
 

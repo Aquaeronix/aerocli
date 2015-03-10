e8c9954b6fbe2a89b3ecfc8656a24eb7369fb235
Dennis Chen <barracks510@gmail.com>
2015-03-09 17:59:06 -0700

    first stable

--- src/aquaero5-offsets.h
+++ src/aquaero5-offsets.h
@@ -1,22 +1,20 @@
 /* Copyright 2012 lynix <lynix47@gmail.com>
  * Copyright 2013 JinTu <JinTu@praecogito.com>, lynix <lynix47@gmail.com>
- * Copyright 2014 barracks510 <barracks510@gmail.com>
- * Copyright 2015 barracks510 <barracks510@gmail.com>
  *
- * This file is part of Aquaeronix.
+ * This file is part of aerotools-ng.
  *
- * Aquaeronix is free software: you can redistribute it and/or modify
+ * aerotools-ng is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Aquaeronix is distributed in the hope that it will be useful,
+ * aerotools-ng is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Aquaeronix. If not, see <http://www.gnu.org/licenses/>.
+ * along with aerotools-ng. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* target firmware to build for */
@@ -26,19 +24,11 @@
 
 /* report lengths */
 #if AQ5_FW_TARGET == 1027
-	#define AQ5_TIME_LEN			4
-	#define AQ5_SOFT_SENSORS_LEN		16
 	#define AQ5_DATA_LEN			659
-	#define AQ5_REPORT_NAME_LEN		523
-	#define AQ5_CLEAN_NAME_LEN		4096
 	#define AQ5_FW_MIN			1027
 	#define AQ5_FW_MAX			1027
 #elif ((AQ5_FW_TARGET <= 1030) && (AQ5_FW_TARGET >= 1028))
-	#define AQ5_TIME_LEN			4
-	#define AQ5_SOFT_SENSORS_LEN		16
 	#define AQ5_DATA_LEN			661
-	#define AQ5_REPORT_NAME_LEN		523
-	#define AQ5_CLEAN_NAME_LEN		4096
 	#define AQ5_FW_MIN			1028
 	#define AQ5_FW_MAX			1030
 #endif
@@ -62,7 +52,6 @@
 	#define AQ5_FLOW_OFFS				0x0fb
 	#define AQ5_LEVEL_OFFS				0x147
 	#define AQ5_FAN_OFFS				0x169
-	#define AQ5_AQUASTREAM_XT_OFFS			0x1c9
 #elif ((AQ5_FW_TARGET <= 1030) && (AQ5_FW_TARGET >= 1028))
 	#define AQ5_CURRENT_TIME_OFFS		0x001
 	#define AQ5_SERIAL_MAJ_OFFS			0x007
@@ -81,7 +70,6 @@
 	#define AQ5_FLOW_OFFS				0x0fd
 	#define AQ5_LEVEL_OFFS				0x149
 	#define AQ5_FAN_OFFS				0x16b
-	#define AQ5_AQUASTREAM_XT_OFFS			0x1cb
 #endif
 
 #define AQ5_TEMP_DIST			2
@@ -92,7 +80,6 @@
 #define AQ5_FLOW_DIST			2
 #define AQ5_CPU_TEMP_DIST		2
 #define AQ5_LEVEL_DIST			2
-#define AQ5_AQUASTREAM_XT_DIST		8
 
 /* data offsets for HID feature report 0xB */
 #define AQ5_SETTINGS_FAN_OFFS					0x20d
@@ -140,7 +127,6 @@
 #define AQ5_SETTINGS_SOFT_SENSOR_OFFS			0x178
 #define AQ5_SETTINGS_FLOW_SENSOR_OFFS			0x1a0
 #define AQ5_SETTINGS_POWER_SENSOR_OFFS			0x1f4
-#define AQ5_SETTINGS_AQUASTREAM_XT_OFFS			0x2fd
 #define AQ5_SETTINGS_CURVE_CONTROLLER_OFFS		0x4f8
 #define AQ5_SETTINGS_TARGET_VAL_CONTRLR_OFFS	0x488
 #define AQ5_SETTINGS_TWO_POINT_CONTRLR_OFFS		0x3e8
@@ -178,96 +164,3 @@
 #define AQ5_SETTINGS_FILL_ALARM_DIST			9
 #define AQ5_SETTINGS_TIMER_DIST					7
 #define AQ5_SETTINGS_INFRARED_DIST				12
-#define AQ5_SETTINGS_AQUASTREAM_XT_DIST			3
-
-#define AQ5_NAME_LEN			24
-
-/* Definitions for name reports */
-typedef struct {
-	uint16_t offset;
-	uint16_t value;
-} name_report_watermark;
-
-name_report_watermark name_report_watermarks[] = {
-	/* 1 
-	 * start 0x0000: 01 00 09 c0 00 00 00 02 00
- 	 * end 0x0209: DD 83
-	 */
-	{ 0x0000,	0x0100 },
-	{ 0x0002,	0x09c0 },
-	{ 0x0004,	0x0000 },
-	{ 0x0006,	0x0002 },
-	{ 0x0007,	0x0200 },
-	{ 0x0209,	0xdd83 },
-	/* 2
-	 * start 0x020b: 01 00 09 C2 00 00 00 02 00
-	 * end 0x0414: DD 84
-	 */
-	{ 0x020b,	0x0100 },
-	{ 0x020d,	0x09c2 },
-	{ 0x020f,	0x0000 },
-	{ 0x0211,	0x0002 },
-	{ 0x0212,	0x0200 },
-	{ 0x0414,	0xdd84 },
-	/* 3
-	 * start 0x0416: 01 00 09 C4 00 00 00 02 00
- 	 * end 0x061f: DD 85
- 	 */
-	{ 0x0416,	0x0100 },
-	{ 0x0418,	0x09c4 },
-	{ 0x041a,	0x0000 },
-	{ 0x041c,	0x0002 },
-	{ 0x041d,	0x0200 },
-	{ 0x061f,	0xdd85 },
-	/* 4
-	 * start 0x0621: 01 00 09 C6 00 00 00 02 00
-	 * end 0x082a: DD 86
-	 */
-	{ 0x0621,	0x0100 },
-	{ 0x0623,	0x09c6 },
-	{ 0x0625,	0x0000 },
-	{ 0x0627,	0x0002 },
-	{ 0x0628,	0x0200 },
-	{ 0x082a,	0xdd86 },
-	/* 5
-	 * start 0x082c: 01 00 09 C8 00 00 00 02 00
-	 * end 0x0a35: DD 87
-	 */
-	{ 0x082c,	0x0100 },
-	{ 0x082e,	0x09c8 },
-	{ 0x0830,	0x0000 },
-	{ 0x0832,	0x0002 },
-	{ 0x0833,	0x0200 },
-	{ 0x0a35,	0xdd87 },
-	/* 6
-  	 * start 0x0a37: 01 00 09 CA 00 00 00 02 00
-	 * end 0x0c40: DD 88
-	 */
-	{ 0x0a37,	0x0100 },
-	{ 0x0a39,	0x09ca },
-	{ 0x0a3b,	0x0000 },
-	{ 0x0a3d,	0x0002 },
-	{ 0x0a3e,	0x0200 },
-	{ 0x0c40,	0xdd88 },
-	/* 7
-	 * start 0x0c42: 01 00 09 CC 00 00 00 02 00
-	 * end 0x0e4b: DD 89
-	 */
-	{ 0x0c42,	0x0100 },
-	{ 0x0c44,	0x09cc },
-	{ 0x0c46,	0x0000 },
-	{ 0x0c48,	0x0002 },
-	{ 0x0c49,	0x0200 },
-	{ 0x0e4b,	0xdd89 },
-	/* 8
-   	 * start 0x0e4d: 01 00 09 CE 00 00 00 02 00
-   	 * end 0x1056 : DD 8A
-	 */
-	{ 0x0e4d,	0x0100 },
-	{ 0x0e4f,	0x09ce },
-	{ 0x0e51,	0x0000 },
-	{ 0x0e53,	0x0002 },
-	{ 0x0e54,	0x0200 },
-	{ 0x1056,	0xdd8a }
-};
-
--- utilities/SNMP/snmp_persist_aquaero5.pl
+++ utilities/SNMP/snmp_persist_aquaero5.pl
@@ -2,33 +2,29 @@
 #
 # snmp_persist_aquaero5.pl
 #
-# Export the Aquaero 5 sensor readings and settings provided ny Aquaeronix to net-snmp via 
+# Export the Aquaero 5 sensor readings and settings provided ny aerotools-ng to net-snmp via 
 # pass_persist for external consumption.
 #
 # Copyright 2013 JinTu <JinTu@praecogito.com>
-# Copyright 2014 barracks510 <barracks510@gmail.com>
-# Copyright 2015 barracks510 <barracks510@gmail.com>
 #
 #
-# This file is a support utility of Aquaeronix.
+# This file is a support utility of aerotools-ng.
 #
 # Copyright 2012 lynix <lynix47@gmail.com>
 # Copyright 2013 JinTu <JinTu@praecogito.com>, lynix <lynix47@gmail.com>
-# Copyright 2014 barracks510 <barracks510@gmail.com>
-# Copyright 2015 barracks510 <barracks510@gmail.com>
 #
-# Aquaeronix is free software: you can redistribute it and/or modify
+# aerotools-ng is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Aquaeronix is distributed in the hope that it will be useful,
+# aerotools-ng is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Aquaeronix. If not, see <http://www.gnu.org/licenses/>.
+# along with aerotools-ng. If not, see <http://www.gnu.org/licenses/>.
 #
 ####
 
@@ -50,9 +46,6 @@
 # Revision history
 ####
 #
-# v0.03 9/1/2013 JinTu <JinTu@praecogito.com>
-# 	Added support for device names.
-#
 # v0.02 4/13/2013 JinTu <JinTu@praecogito.com>
 # 	Adding support for virtual, software and other sensors.
 #
@@ -71,8 +64,6 @@
 #
 # Temperature sensors
 # .1.3.6.1.4.1.2021.255.65.67.1.1.2
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.2.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.2.1
 #   Temp value
@@ -80,8 +71,6 @@
 #
 # Fans
 # .1.3.6.1.4.1.2021.255.65.67.1.1.3
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.3.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.3.1
 #   VRM temp
@@ -97,8 +86,6 @@
 #
 # Flow sensors
 # .1.3.6.1.4.1.2021.255.65.67.1.1.4
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.4.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.4.1
 #   Flow value
@@ -106,8 +93,6 @@
 #
 # CPU temperatures
 # .1.3.6.1.4.1.2021.255.65.67.1.1.5
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.5.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.5.1
 #   CPU temp
@@ -115,17 +100,13 @@
 
 # Level sensors
 # .1.3.6.1.4.1.2021.255.65.67.1.1.6
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.6.0
 #   Name
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.6.1
+#   .1.3.6.1.4.1.2021.255.65.67.1.1.6
 #   Level value
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.6.2
+#   .1.3.6.1.4.1.2021.255.65.67.1.1.6
 #
 # Virtual sensors
 # .1.3.6.1.4.1.2021.255.65.67.1.1.7
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.7.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.7.1
 #   Virtual sensor value
@@ -133,8 +114,6 @@
 #
 # Software sensors
 # .1.3.6.1.4.1.2021.255.65.67.1.1.8
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.8.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.8.1
 #   Software sensor value
@@ -142,8 +121,6 @@
 #
 # Other sensors
 # .1.3.6.1.4.1.2021.255.65.67.1.1.9
-#   Device
-#   .1.3.6.1.4.1.2021.255.65.67.1.1.9.0
 #   Name
 #   .1.3.6.1.4.1.2021.255.65.67.1.1.9.1
 #   Other sensor value
@@ -263,19 +240,14 @@ sub create_mib {
 			print "Line->$line<-\n" if $debug;
 			$tempval = 0 if ($tempval < 0);
 			print "Temp $tempnum = $tempval (" . strip_decimal($tempval) . ")\n" if $debug;
-			$tmpmib{"1.1.2.0.0.$tempnum"} = [ "string", "Temp$tempnum" ];
+			$tmpmib{"1.1.2.1.0.$tempnum"} = [ "string", "Temp$tempnum" ];
 			$tmpmib{"1.1.2.2.0.$tempnum"} = [ "gauge", adjust_to_32bit(strip_decimal($tempval)) ];
-		} elsif ($line =~ /^TEMP\d+_NAME=/) {
-			my ($tempnum,$tempname) = $line =~ /^TEMP(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Temp $tempnum name = \"$tempname\"\n" if $debug;
-			$tmpmib{"1.1.2.1.0.$tempnum"} = [ "string", "$tempname" ];
 		} elsif ($line =~ /^FAN\d+_VRM_TEMP=/) {
 			my ($fannum,$tempval) = $line =~ /^FAN(\d+)_VRM_TEMP=(-?\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
 			$tempval = 0 if ($tempval < 0);
 			print "Fan $fannum VRM temp = $tempval (" . strip_decimal($tempval) . ")\n" if $debug;
-			$tmpmib{"1.1.3.0.0.$fannum"} = [ "string", "Fan$fannum" ];
+			$tmpmib{"1.1.3.1.0.$fannum"} = [ "string", "Fan$fannum" ];
 			$tmpmib{"1.1.3.2.0.$fannum"} = [ "gauge", adjust_to_32bit(strip_decimal($tempval)) ];
 		} elsif ($line =~ /^FAN\d+_CURRENT=/) {
 			my ($fannum,$currentval) = $line =~ /^FAN(\d+)_CURRENT=(\d+)/i;
@@ -297,68 +269,38 @@ sub create_mib {
 			print "Line->$line<-\n" if $debug;
 			print "Fan $fannum duty cycle = $voltageval (" . strip_decimal($voltageval) . ")\n" if $debug;
 			$tmpmib{"1.1.3.6.0.$fannum"} = [ "gauge", adjust_to_32bit(strip_decimal($voltageval)) ];
-		} elsif ($line =~ /^FAN\d+_NAME=/) {
-			my ($fannum,$fanname) = $line =~ /^FAN(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Fan $fannum name = \"$fanname\"\n" if $debug;
-			$tmpmib{"1.1.3.1.0.$fannum"} = [ "string", "$fanname" ];
 		} elsif ($line =~ /^FLOW\d+=/) {
 			my ($flownum,$flowval) = $line =~ /^FLOW(\d+)=(\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
 			print "Flow sensor $flownum = $flowval (" . strip_decimal($flowval) . ")\n" if $debug;
-			$tmpmib{"1.1.4.0.0.$flownum"} = [ "string", "Flow$flownum" ];
+			$tmpmib{"1.1.4.1.0.$flownum"} = [ "string", "Flow$flownum" ];
 			$tmpmib{"1.1.4.2.0.$flownum"} = [ "gauge", adjust_to_32bit(strip_decimal($flowval)) ];
-		} elsif ($line =~ /^FLOW\d+_NAME=/) {
-			my ($flownum,$flowname) = $line =~ /^FLOW(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Flow sensor $flownum name = \"$flowname\"\n" if $debug;
-			$tmpmib{"1.1.4.1.0.$flownum"} = [ "string", "$flowname" ];
 		} elsif ($line =~ /^SYS_TEMP_CPU\d+=/) {
 			my ($cpunum,$tempval) = $line =~ /^SYS_TEMP_CPU(\d+)=(\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
 			print "CPU $cpunum = $tempval (" . strip_decimal($tempval) . ")\n" if $debug;
-			$tmpmib{"1.1.5.0.0.$cpunum"} = [ "string", "CPUTemp$cpunum" ];
+			$tmpmib{"1.1.5.1.0.$cpunum"} = [ "string", "CPUTemp$cpunum" ];
 			$tmpmib{"1.1.5.2.0.$cpunum"} = [ "gauge", adjust_to_32bit(strip_decimal($tempval)) ];
-		} elsif ($line =~ /^SYS_TEMP_CPU\d+_NAME=/) {
-			my ($cpunum,$cpuname) = $line =~ /^SYS_TEMP_CPU(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "CPU $cpunum name = \"$cpuname\"\n" if $debug;
-			$tmpmib{"1.1.5.1.0.$cpunum"} = [ "string", "$cpuname" ];
 		} elsif ($line =~ /^LEVEL\d+=/) {
 			my ($levelnum,$levelval) = $line =~ /^LEVEL(\d+)=(\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
 			print "Level sensor $levelnum = $levelval (" . strip_decimal($levelval) . ")\n" if $debug;
-			$tmpmib{"1.1.6.0.0.$levelnum"} = [ "string", "Level$levelnum" ];
+			$tmpmib{"1.1.6.1.0.$levelnum"} = [ "string", "Level$levelnum" ];
 			$tmpmib{"1.1.6.2.0.$levelnum"} = [ "gauge", adjust_to_32bit(strip_decimal($levelval)) ];
-		} elsif ($line =~ /^LEVEL\d+_NAME=/) {
-			my ($levelnum,$levelname) = $line =~ /^LEVEL(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Level $levelnum name = \"$levelname\"\n" if $debug;
-			$tmpmib{"1.1.6.1.0.$levelnum"} = [ "string", "$levelname" ];
 		} elsif ($line =~ /^VIRT_TEMP\d+=/) {
 			my ($tempnum,$tempval) = $line =~ /^VIRT_TEMP(\d+)=(-?\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
 			$tempval = 0 if ($tempval < 0);
 			print "Virtual sensor temp $tempnum = $tempval (" . strip_decimal($tempval) . ")\n" if $debug;
-			$tmpmib{"1.1.7.0.0.$tempnum"} = [ "string", "VirtualTemp$tempnum" ];
+			$tmpmib{"1.1.7.1.0.$tempnum"} = [ "string", "VirtualTemp$tempnum" ];
 			$tmpmib{"1.1.7.2.0.$tempnum"} = [ "gauge", adjust_to_32bit(strip_decimal($tempval)) ];
-		} elsif ($line =~ /^VIRT_TEMP\d+_NAME=/) {
-			my ($tempnum,$tempname) = $line =~ /^VIRT_TEMP(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Virtual sensor temp $tempnum name = \"$tempname\"\n" if $debug;
-			$tmpmib{"1.1.7.1.0.$tempnum"} = [ "string", "$tempname" ];
 		} elsif ($line =~ /^SOFT_TEMP\d+=/) {
 			my ($tempnum,$tempval) = $line =~ /^SOFT_TEMP(\d+)=(-?\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
 			$tempval = 0 if ($tempval < 0);
 			print "Software sensor temp $tempnum = $tempval (" . strip_decimal($tempval) . ")\n" if $debug;
-			$tmpmib{"1.1.8.0.0.$tempnum"} = [ "string", "SoftwareTemp$tempnum" ];
+			$tmpmib{"1.1.8.1.0.$tempnum"} = [ "string", "SoftwareTemp$tempnum" ];
 			$tmpmib{"1.1.8.2.0.$tempnum"} = [ "gauge", adjust_to_32bit(strip_decimal($tempval)) ];
-		} elsif ($line =~ /^SOFT_TEMP\d+_NAME=/) {
-			my ($tempnum,$tempname) = $line =~ /^SOFT_TEMP(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Software sensor temp $tempnum name = \"$tempname\"\n" if $debug;
-			$tmpmib{"1.1.8.1.0.$tempnum"} = [ "string", "$tempname" ];
 		} elsif ($line =~ /^OTHER_TEMP\d+=/) {
 			my ($tempnum,$tempval) = $line =~ /^OTHER_TEMP(\d+)=(-?\d+\.\d+)/i;
 			print "Line->$line<-\n" if $debug;
@@ -366,11 +308,6 @@ sub create_mib {
 			print "Other sensor temp $tempnum = $tempval (" . strip_decimal($tempval) . ")\n" if $debug;
 			$tmpmib{"1.1.9.1.0.$tempnum"} = [ "string", "OtherTemp$tempnum" ];
 			$tmpmib{"1.1.9.2.0.$tempnum"} = [ "gauge", adjust_to_32bit(strip_decimal($tempval)) ];
-		} elsif ($line =~ /^OTHER_TEMP\d+_NAME=/) {
-			my ($tempnum,$tempname) = $line =~ /^OTHER_TEMP(\d+)_NAME='(.*)'$/i;
-			print "Line->$line<-\n" if $debug;
-			print "Other sensor temp $tempnum name = \"$tempname\"\n" if $debug;
-			$tmpmib{"1.1.9.1.0.$tempnum"} = [ "string", "$tempname" ];
 		}
 	}
 	$mib = \%tmpmib;

0580a011dd914ae4d0156e8175a59e7916350010
Dennis Chen <barracks510@gmail.com>
2015-03-13 23:56:10 -0700

    update headers and clean

--- src/aerocli.c
+++ src/aerocli.c
@@ -1,20 +1,22 @@
 /* Copyright 2012 lynix <lynix47@gmail.com>
  * Copyright 2013 JinTu <JinTu@praecogito.com>, lynix <lynix47@gmail.com>
+ * Copyright 2014 barracks510 <barracks510@gmail.com>
+ * Copyright 2015 barracks510 <barracks510@gmail.com>
  *
- * This file is part of aerotools-ng.
+ * This file is part of Aquaeronix.
  *
- * aerotools-ng is free software: you can redistribute it and/or modify
+ * Aquaeronix is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * aerotools-ng is distributed in the hope that it will be useful,
+ * Aquaeronix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with aerotools-ng. If not, see <http://www.gnu.org/licenses/>.
+ * along with Aquaeronix. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
